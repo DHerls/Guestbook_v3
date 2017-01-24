d7a2a7dad16c036d3ee88e14ed6900d6e4ff5090
Dan Herlihy <djherls@gmail.com>
2017-01-23 21:40:22 -0500

    Add realistic member/guest seeding with price integration

--- database/seeds/GuestSeeder.php
+++ database/seeds/GuestSeeder.php
@@ -16,8 +16,28 @@ class GuestSeeder extends Seeder
         factory(GuestRecord::class, 1000)->create()->each(function($gr) use($faker){
             foreach (range(1,random_int(1,4)) as $index){
                 $gr->guests()->save(factory(\App\Guest::class)->make(), ['free_pass' => random_int(1,4) == 1,
-                    'created_at' => $faker->dateTimeThisMonth($max = 'now', $timezone = date_default_timezone_get())]);
+                    'created_at' => $gr->created_at]);
             }
         });
+
+        $records = GuestRecord::all();
+        foreach ($records as $record){
+            if (date('N', strtotime($record->created_at)) >= 6){
+                $record->price = 15 * $record->guests()->whereType('adult')->wherePivot('free_pass', '=', 0)->count() +
+                    5 * $record->guests()->whereType('child')->wherePivot('free_pass', '=', 0)->count();
+            } else {
+                $record->price = 10 * $record->guests()->whereType('adult')->wherePivot('free_pass', '=', 0)->count() +
+                    5 * $record->guests()->whereType('child')->wherePivot('free_pass', '=', 0)->count();
+            }
+            if ($record->payment_method == 'account'){
+                $br = new \App\BalanceRecord();
+                $br->change_amount = $record->price;
+                $br->user_id = $record->user_id;
+                $br->member_id = $record->member_id;
+                $br->reason = "Guest Check-In";
+                $record->balanceRecord()->save($br);
+            }
+            $record->save();
+        }
     }
 }
